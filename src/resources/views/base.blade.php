bbea3d8d9c266a227b246be44a34d7c6d1b20e69
omaar-es <omar.redondo.gonzalez.1b@gmail.com>
2025-12-04 13:57:08 -0600

    refactor: Improve login form layout and accessibility with Bootstrap components

--- src/resources/views/base.blade.php
+++ src/resources/views/base.blade.php
@@ -1,16 +1,17 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-    
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" href="/assets/icon.png" type="image/icon type">    
+    <link rel="icon" href="/assets/icon.png" type="image/icon type">
     <title>{{ config('app.name') }}</title>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 
 <body>
-  @include('caronte::messages')
+    @include('caronte::messages')
     <main role="main">
         @yield('content')
     </main>
